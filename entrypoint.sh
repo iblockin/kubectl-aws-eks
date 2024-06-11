5ab48bd81de7222172dcc0573e16c651b470c73c
zongyou <thrmyn@users.noreply.github.com>
2024-06-11 22:29:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+cat /tmp/config
+
 export KUBECONFIG=/tmp/config
 
 if [ -z ${KUBECTL_VERSION+x} ] ; then
